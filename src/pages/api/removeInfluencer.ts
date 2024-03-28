acd43fb87eae18beea743b63e62763a6d7fb5b12
Malik Öztürk <malik.ozturk@enuygun.com>
2024-03-28 21:13:45 +0300

    feat: add remove influencer api

---
+++ src/pages/api/removeInfluencer.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/lib/prisma";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    await prisma.Influencer.delete({
+      where: {
+        id: req.body.id,
+      },
+    });
+    return res.status(200).json("Influencer başarıyla silindi.");
+  } catch (error) {
+    console.log(error, "error");
+    return res.status(200).json("Id'ye ait kullanıcı bulunamadı");
+  }
+};
