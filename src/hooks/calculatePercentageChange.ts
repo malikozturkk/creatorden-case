7ad4abc15a87f17593441846b9eed20ebfde1356
Malik Öztürk <malik.ozturk@enuygun.com>
2024-03-30 22:37:18 +0300

    change: calculatePercentageChange function

--- src/hooks/calculatePercentageChange.ts
+++ src/hooks/calculatePercentageChange.ts
@@ -10,12 +10,14 @@ interface DataItem {
 export function calculatePercentageChange(data: DataItem[]): DataItem[] {
   data.forEach((item, index) => {
     if (index === 0) {
-      item.change = null;
+      item.change = null; // İlk öğe için değişim hesaplanamaz
     } else {
       const previousItem = data[index - 1];
       const percentageChange =
-        item.percentage_rate - previousItem.percentage_rate;
-      item.change = percentageChange.toFixed(2);
+        ((item.percentage_rate - previousItem.percentage_rate) /
+          previousItem.percentage_rate) *
+        100;
+      item.change = percentageChange.toFixed(1);
     }
   });
 
