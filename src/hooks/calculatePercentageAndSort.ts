73e530b6eb92863699477410d7fb47ceba815e90
Malik Öztürk <malik.ozturk@enuygun.com>
2024-03-30 22:12:41 +0300

    chore: add calculatePercentageAndSort function

--- src/hooks/calculatePercentageAndSort.ts
+++ src/hooks/calculatePercentageAndSort.ts
@@ -3,6 +3,7 @@ interface DataItem {
   count: number;
   total_reach_rate: number;
   percentage_rate?: number;
+  highest?: boolean;
 }
 
 export function calculatePercentageAndSort(data: DataItem[]): DataItem[] {
@@ -10,9 +11,20 @@ export function calculatePercentageAndSort(data: DataItem[]): DataItem[] {
     (acc, item) => acc + item.total_reach_rate,
     0
   );
+
   const updatedData = data.map((item) => ({
     ...item,
     percentage_rate: (item.total_reach_rate / totalReachRate) * 100,
   }));
-  return updatedData.sort((a, b) => a.year - b.year);
+
+  const highestPercentageRate = Math.max(
+    ...updatedData.map((item) => item.percentage_rate || 0)
+  );
+
+  const finalData = updatedData.map((item) => ({
+    ...item,
+    highest: item.percentage_rate === highestPercentageRate,
+  }));
+
+  return finalData.sort((a, b) => a.year - b.year);
 }
