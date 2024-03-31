2ca99f1c9a6d814b9605a959ff06112d43c5cf69
Malik Öztürk <malik.ozturk@enuygun.com>
2024-03-31 15:40:58 +0300

    feat: add useMemo in app tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import React, { useMemo } from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -8,18 +9,27 @@ import { AlertProvider } from "@/context/useAlert";
 import { TopThreeProvider } from "@/context/useTopThree";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
-  const darkTheme = createTheme({
-    palette: {
-      mode: "dark",
-    },
-  });
+  const queryClient = useMemo(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+    []
+  );
+
+  const darkTheme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: "dark",
+        },
+      }),
+    []
+  );
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={darkTheme}>
